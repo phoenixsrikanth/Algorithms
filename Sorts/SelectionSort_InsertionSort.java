e7a115a02da06914323f1f300d4088a8d4b9fd0b
Srikanth Goli <gsrikanth22@outlook.com>
2017-01-02 19:48:25 -0600

    Selection Sort and Insertion Sort in Java
    Implementation of Selection Sort and Insertion Sort in Java.

--- Sorts/SelectionSort_InsertionSort.java
+++ Sorts/SelectionSort_InsertionSort.java
@@ -1,6 +1,27 @@
-package sorts;
 import java.util.Arrays;
 import java.util.*;
+
+/* 
+	Selection Sort: Sorts integers with O(N^2) runtime.
+	Insertion Sort: Sorts integers with O(N^2) runtime on average.
+	While the best runtime for Insertion Sort is O(N).
+	
+	Copyright (C) 2017 Srikanth Goli (https://github.com/phoenixsrikanth/)
+	
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 public class Selection_Insertion_Sort {
 
